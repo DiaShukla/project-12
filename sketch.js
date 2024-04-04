53588dffb5e5a1da33a143921e29871542a313e4
DiaShukla <163628050+DiaShukla@users.noreply.github.com>
2024-04-04 21:13:09 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var marks = [35,38,42,45,43,34,46,41,48,32];
+
+
+function setup() 
+{
+for(i=0;i < marks.length;i++){
+  if(marks[i]== 45){
+console.log("45 is present in the array");
+  }
+}
+
+
+  createCanvas(400,400);
+
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
